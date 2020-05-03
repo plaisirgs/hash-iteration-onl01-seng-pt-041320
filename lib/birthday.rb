3f27554c5f94a536ef6d958a0ff494971768ae2b
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-03 20:25:29 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,41 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+def select_winner(passengers)
+  passengers[:suite_a]
+end
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |kid, age|
+    puts "Happy Birthday #{kid}! You are now #{age} years old!"
+  end
+end
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kid, age|
+    if age <= 12
+      puts "Happy Birthday #{kid}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
+    end
+  end
 end
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# my_hash = {"key" => "value"}
+# my_hash["door"]="open"
+# # key: "value"
+# :key => "value"
